5c332eabdd7232db6f04c7c459d6da50e784bf50
NguyenHuuNinh <nguyenhuuninh11032006@gmail.com>
2026-04-07 23:01:40 +0700

    update

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-// Cấu hình để server tự động đọc file index.html trong thư mục public
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(port, () => {
